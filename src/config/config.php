b430f004c91147cbfd60b5d062eaa48a856c263e
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-01-23 14:15:09 +0100

    added first file for db connection

---
+++ src/config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = '127.0.0.1';       
+$db   = 'schema_relazionale_aggiornato'; 
+$user = 'root';            
+$pass = 'passwordMsw';     
+$port = '3306';            
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
